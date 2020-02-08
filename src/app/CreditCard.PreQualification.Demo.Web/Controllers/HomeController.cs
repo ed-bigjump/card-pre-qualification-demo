e93ecac3d0f9a6916832e700f045c35b03e8258c
Edward Moss <ed@bigjump.co.uk>
2020-02-08 16:17:39 +0000

    Map DOB, style validation summary

--- src/app/CreditCard.PreQualification.Demo.Web/Controllers/HomeController.cs
+++ src/app/CreditCard.PreQualification.Demo.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +18,8 @@ namespace CreditCard.PreQualification.Demo.Web.Controllers
         [HttpPost]
         public IActionResult Index(IndexPostModel model)
         {
+            //TODO validate DOB is correct date
+
             if (!ModelState.IsValid) return Index();
 
             return Index();
@@ -31,9 +34,17 @@ namespace CreditCard.PreQualification.Demo.Web.Controllers
 
     public class IndexPostModel
     {
+        [Required(ErrorMessage = "Please enter your first name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage =  "Please enter your last name")]
         public string LastName { get; set; }
-        public DateTime? DateOfBirth { get; set; }
-        public int AnnualIncome { get; set; }
+
+        public int? BirthDay { get; set; }
+        public int? BirthMonth { get; set; }
+        public int? BirthYear { get; set; }
+
+        [Required(ErrorMessage =  "Please enter your annual income")]
+        public int? AnnualIncome { get; set; }
     }
 }

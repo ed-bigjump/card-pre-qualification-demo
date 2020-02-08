c0865929bd332aacc6b8272b9eb8ab72f8654e92
Edward Moss <ed@bigjump.co.uk>
2020-02-08 15:49:57 +0000

    Wireframe entry/result screens

---
+++ src/app/CreditCard.PreQualification.Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditCard.PreQualification.Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IndexPostModel model)
+        {
+            if (!ModelState.IsValid) return Index();
+
+            return Index();
+        }
+
+        [HttpGet]
+        public IActionResult Recommendation()
+        {
+            return View();
+        }
+    }
+
+    public class IndexPostModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public int AnnualIncome { get; set; }
+    }
+}
